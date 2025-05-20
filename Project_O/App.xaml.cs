28be4e21add09a6879c4a0951c5ce9c97b9a0cf7
NTC539 <gamingdimon274@gmail.com>
2025-05-20 10:15:45 +0300

    Dmitriy added fix

--- Project_O/App.xaml.cs
+++ Project_O/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using TaskManagerLogic;
 
 namespace Project_O
 {
@@ -14,4 +9,5 @@ namespace Project_O
     public partial class App : Application
     {
     }
+
 }
--- Project_O/TaskManagerLogicClasses/User.cs
+++ Project_O/TaskManagerLogicClasses/User.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using YandexDisk;
 using System.Security.Cryptography;
 using System.Text;
+using System.IO;
 
 namespace TaskManagerLogic.Classes
 {
