203017a4badb61facd4a564bc6b34647897301bc
NTC539 <gamingdimon274@gmail.com>
2025-05-26 10:23:26 +0300

    Start window user functions added

--- Project_O/View/Start.xaml.cs
+++ Project_O/View/Start.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using TaskManagerLogic.Classes;
 
 namespace Project_O.Windows
 {
@@ -34,6 +35,38 @@ namespace Project_O.Windows
             {
                 LogIn.Visibility = Visibility.Visible;
                 Register.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        // UserException с кодом ошибки 1 - такого пользователя нет
+        // UserException с кодом ошибки 2 - неверный пароль 
+        // UserException с кодом ошибки 3 - пользователь с таким ником уже существует
+        // Для обработки catch (UserException ex) when (ex.ErrorCode == N)
+        private async void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await User.Login(UserNameLoginTextBox.Text, PasswordLoginTextBox.maskedTextBox.Text);
+            }
+            catch (UserException ex) when (ex.ErrorCode == 1)
+            {
+
+            }
+            catch (UserException ex) when (ex.ErrorCode == 2)
+            {
+
+            }
+        }
+
+        private async void Button_Register_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await User.Register(UserNameRegisterTextBox.Text, PasswordRegisterTextBox.maskedTextBox.Text);
+            }
+            catch (UserException ex) when (ex.ErrorCode == 3)
+            {
+
             }
         }
     }
