43b4b9a91474103c34af488034d94a5253bf443c
ed1ct7 <ediktigranian@gmail.com>
2025-05-27 22:37:56 +0300

    Picture displayed in textbox

--- Project_O/Classes/TagProperties.cs
+++ Project_O/Classes/TagProperties.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Project_O.Classes
 {
-    internal class TagProperties
+    public static class TagProperties
     {
+        public static readonly DependencyProperty CustomTagProperty =
+                DependencyProperty.RegisterAttached(
+                    "CustomTag",
+                    typeof(string),
+                    typeof(TagProperties),
+                    new PropertyMetadata(""));
+
+        public static string GetCustomTag(DependencyObject obj)
+        {
+            return (string)obj.GetValue(CustomTagProperty);
+        }
+
+        public static void SetCustomTag(DependencyObject obj, string value)
+        {
+            obj.SetValue(CustomTagProperty, value);
+        }
     }
 }
